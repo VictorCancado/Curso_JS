0c8f78e1fc9e11cfce90d7f110a3e31dbc31fe75
Víctor Cançado <victor.cancado@gmail.com>
2022-12-09 10:59:04 -0300

    Aula 4

---
+++ aula4/comentarios.js
@@ -0,0 +1,8 @@
+// Escrever um comentário
+console.log("Olá, Mundo!!!"); // Aqui tem outro comentário
+console.log('Linha 1');
+// Comentário
+console.log("Linha 2");
+
+/* Um bloco
+de comentários */
