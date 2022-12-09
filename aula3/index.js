6f087bcebd488a06aab8cf33039a030bd368ac64
Víctor Cançado <victor.cancado@gmail.com>
2022-12-09 10:39:07 -0300

    Aula 3

--- aula3/index.js
+++ aula3/index.js
@@ -1,7 +1,1 @@
-console.log('Víctor "Cançado"');
-console.log("Víctor 'Cançado'");
-console.log(`'Víctor' "Cançado"`);
-
-console.log(123456);
-
-console.log(123, 45.89, "Víctor Cançado");
+console.log(`Meu nome é "Víctor". Estou aprendendo Javascript às ${10} da manhã`);
